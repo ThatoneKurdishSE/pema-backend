f6a4d08512e77bbb5f2e4479e49ab96d2b740c6a
ThatoneKurdishSE <razaljaf@gmail.com>
2021-03-23 19:57:10 -0600

    auth established and meditation route secured

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,7 +1,20 @@
 class MeditationsController < ApplicationController
     def index
-        @meditations = Meditation.all
+        authrization_header = request.headers["authorization"]
+        token = authrization_header.split(" ")[1]
+        if !token
+            render json: { error: "Not authorized!" }, status: :unauthorized
+        else
+            secret = "TOP SECRET"
+            begin 
+            payload = JWT.decode(token, secret).first
+                @user = User.find(payload["user_id"])
+            @meditations = Meditation.all
 
-        render json: { meditations: @meditations }
+            render json: { meditations: @meditations }
+            rescue
+                render json: { error: "Not authorized!" }, status: :unauthorized
+            end
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   post 'users', to: "users#create"
   post 'login', to: "authentication#login"
   
-  resource :meditations, only: [:index]
+  resources :meditations, only: [:index]
 end
