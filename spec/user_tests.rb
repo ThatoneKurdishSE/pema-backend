5b662c42f95d73cf352fa9697daf2b9d66140839
Raz <razaljaf@gmail.com>
2021-05-18 07:31:03 -0600

    Rspec gem installed, 1 test failure in progress.

---
+++ spec/user_tests.rb
@@ -0,0 +1,7 @@
+describe username_validation do
+    context "When ran successfully username_validation method" do
+        
+        it "should validate if name presence == true, username presence === true"
+    end
+end
+end
